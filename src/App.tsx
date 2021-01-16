4673713cc0a72378055f85954046340eb123fc5e
nagchanallen <nagisawags@gmail.com>
2021-01-17 04:32:34 +0900

    created structure of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
+import GamePicker from './components/GamePicker';
+import Games from './components/Games';
 
-const App = () => <div>Mahjong Tracker</div>;
+const App: React.FC = () => {
+  return (
+    <div>
+      <GamePicker />
+      <Games />
+    </div>
+  );
+};
 
 export default hot(module)(App);
