6978f6b9a2befb617dfbe0a02b85ee59669f39f9
nagchanallen <nagisawags@gmail.com>
2021-01-18 19:46:12 +0900

    imported gameListInfo & decoder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { hot } from 'react-hot-loader';
 import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { hot } from 'react-hot-loader';
+import { Helmet } from 'react-helmet';
 import GamePicker from './components/GamePicker/GamePicker';
 import Games from './components/Games/Games';
 import './App.css';
 
+declare global {
+  interface Window {
+    sw: any;
+  }
+}
+
 const App: React.FC = () => {
+  const isFirstRendered = useRef(true);
+  const [gameList, setGameList] = useState([]);
+
+  if (isFirstRendered.current) {
+    window.sw = (arr: string[]) => {
+      setGameList(arr);
+    };
+    isFirstRendered.current = false;
+  }
+
   return (
     <div>
+      {gameList.map((e) => (
+        <div>{e}</div>
+      ))}
+      <Helmet>
+        <script src="https://tenhou.net/lib/base64.js" type="text/javascript" />
+        <script src="https://mjv.jp/0/wg/0.js" type="text/javascript" />
+      </Helmet>
       <div>
         <GamePicker />
       </div>
