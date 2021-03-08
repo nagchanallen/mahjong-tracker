8af327af54054033816aa5317ad504145313b888
nagchanallen <nagisawags@gmail.com>
2021-03-08 19:43:42 +0900

    adjusted mainWindow size

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 760,
-    width: 370,
+    height: 800,
+    width: 400,
   });
   mainWindow.removeMenu();
 
